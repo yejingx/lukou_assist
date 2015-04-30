891175c1998cef15f11dab711957d6bdaeda63d9
yejingx <yejingx@gmail.com>
2015-04-30 19:01:50 +0800

    query

--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
 document.querySelector('#share-box button').addEventListener('click',
   function () {
-    chrome.tabs.getSelected(function (tab) {
+    chrome.tabs.query({active: true}, function (tabs) {
         chrome.tabs.create({
-          'url':'http://www.lukou.com/post/commodity?url='+encodeURIComponent(tab.url),
+          'url':'http://www.lukou.com/post/commodity?url='+encodeURIComponent(tabs[0].url),
           'selected':true
         });
     });
-    window.close();
 });
